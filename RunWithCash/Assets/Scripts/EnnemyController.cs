436d35db2a4b6b6d3cd5ad5b06d2599fada6f9a8
Pob-Fr <po.brocquet@gmail.com>
2016-11-11 12:06:43 +0100

    Script IA + projectile
    Ajout script IA
    Ajout script projectile

---
+++ RunWithCash/Assets/Scripts/EnnemyController.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody))]
+public class NewBehaviourScript : MonoBehaviour {
+	public Transform target;
+	int lifePoint = 1;
+	bool inScreen;
+	bool alive = true;
+	float speed;
+
+	public float maxRectif = 15;
+	public float maxSpeed = 100;
+
+	void Update () {
+		if (lifePoint <= 0) {
+			alive = false;
+		}
+	}
+
+	void changeDirection () {
+		float angleBetweenTarget = Vector3.Angle (transform.forward, transform.forward);
+		if (angleBetweenTarget != Mathf.Abs(5.0f)) {
+			// Rectifier la direction
+		}
+
+	}
+
+	void getHit (Projectile projectile) {
+		lifePoint -= projectile.power;
+	}
+}
---
+++ RunWithCash/Assets/Scripts/Projectile.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof (Rigidbody))]
+public class Projectile : MonoBehaviour {
+
+	Rigidbody rb;
+	public int power;
+	float weight;
+	Vector3 direction;
+	float strenght;
+
+
+	// Use this for initialization
+	void Start () {
+		rb = GetComponent<Rigidbody> ();
+		Vector3 throwVector = new Vector3 (strenght * Mathf.Cos(45),strenght * Mathf.Sin(45), 0);
+		transform.Rotate(direction);
+		rb.AddForce(throwVector);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
