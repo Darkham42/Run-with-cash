312415f4b3f47e1fab274c30a4add02dc7acbd72
Pob-Fr <po.brocquet@gmail.com>
2016-11-12 16:17:11 +0100

    Dynamite amelio

--- RunWithCash/Assets/Scripts/Projectile/FireController.cs
+++ RunWithCash/Assets/Scripts/Projectile/FireController.cs
@@ -8,7 +8,7 @@ public class FireController : MonoBehaviour {
 	private Gun gun;
 	public Projectile item;
 	float RayDistance; 
-	public float maxRange = 8;
+	public float maxRange = 15;
 	public float rectifSpeedCar = 30;
 	int layerMask;
 
@@ -20,13 +20,22 @@ public class FireController : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+		Debug.DrawRay (transform.position, Vector3.back * maxRange, Color.red);
+		/*
+		RaycastHit hit2;
+		if (Physics.BoxCast (transform.position, new Vector3 (1, 1, 1), Vector3.back, out hit2, new Quaternion (0, 0, 0, 0), maxRange)) {
+			Debug.Log ("A toi de tirer !");
+			Debug.Break ();
+		}
+		*/
 		if (Input.GetButtonDown ("Fire1")) {
 			RaycastHit hit;
 			Projectile newProjectile = Instantiate (item, transform.position, transform.rotation) as Projectile;
 
 			newProjectile.GetComponent<Rigidbody> ().AddForce (Vector3.forward * rectifSpeedCar, ForceMode.Impulse);
 			newProjectile.distance = maxRange;
-			if (Physics.Raycast (transform.position, Vector3.back, out hit, maxRange)) {
+			if (Physics.BoxCast (transform.position, new Vector3 (1, 1, 1), Vector3.back, out hit, new Quaternion (0, 0, 0, 0), maxRange)) {
+				Debug.Log ("Police a portee.");
 				newProjectile.distance = hit.distance;
 			}
 		}
