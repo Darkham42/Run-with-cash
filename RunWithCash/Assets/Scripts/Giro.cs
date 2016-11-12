0d8e1660f24903efaa376821a565b9c36e1ca013
Darkham <thomas.picard666@gmail.com>
2016-11-12 15:54:32 +0100

    Gyrophare is cool !

--- RunWithCash/Assets/Scripts/Giro.cs
+++ RunWithCash/Assets/Scripts/Giro.cs
@@ -3,13 +3,23 @@ using System.Collections;
 
 public class Giro : MonoBehaviour {
 
+    GameObject LeftLight;
+    GameObject RightLight;
+    float timer = 0;
+
 	// Use this for initialization
 	void Start () {
-	
-	}
+        LeftLight = transform.FindChild("GyroBlue").gameObject;
+        RightLight = transform.FindChild("GyroRed").gameObject;
+    }
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(0, 125 * Time.deltaTime, 0);
+        timer += Time.deltaTime;
+        if (timer > 0.5f) {
+            LeftLight.SetActive(!LeftLight.activeSelf);
+            RightLight.SetActive(!RightLight.activeSelf);
+            timer = 0;
+        }
 	}
 }
