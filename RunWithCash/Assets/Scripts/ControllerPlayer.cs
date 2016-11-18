9dc50c62f9eeec22983d4f5e64bc9b78c0ff484d
Pob-Fr <po.brocquet@gmail.com>
2016-11-18 23:53:37 +0100

    Move + rotation
    Déplacement 4 axes
    Rotation avec mouvement <- A refaire pour rajouter le retour de force.

---
+++ RunWithCash/Assets/Scripts/ControllerPlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(CarBehaviour))]
+public class ControllerPlayer : MonoBehaviour {
+
+	float speedUp;
+	float turn;
+	CarBehaviour carBehaviour;
+
+	// Use this for initialization
+	void Start () {
+		carBehaviour = GetComponent <CarBehaviour> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		speedUp = Input.GetAxisRaw ("Vertical");
+		turn = Input.GetAxisRaw ("Horizontal");
+
+		carBehaviour.inputSpeed (speedUp);
+		carBehaviour.inputDirection (turn);
+	}
+}
