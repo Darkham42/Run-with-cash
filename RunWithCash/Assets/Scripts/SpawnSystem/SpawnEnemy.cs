a066ce4800ac7342bae57390fa0cd61c76806a98
Pob-Fr <po.brocquet@gmail.com>
2016-12-02 23:59:32 +0100

    Day 1

---
+++ RunWithCash/Assets/Scripts/SpawnSystem/SpawnEnemy.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SpawnEnemy : MonoBehaviour {
+	float[] posCars = new float[2];
+//	List<float> posCars2;
+
+	void Start () {
+		posCars [0] = -3;
+		posCars [1] = 6;
+		Debug.Log (posCars[0] + " / " + posCars[1]);
+	}
+
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.R))
+			Debug.Log ("Test choose option : " + choosePosition (-10f, 10f));
+	}
+
+	float choosePosition (float min, float max) {
+		float rand = Random.Range(min, max - posCars.Length * 2) ;
+		Debug.Log ("Rand : " + rand);
+		foreach (float car in posCars) {
+			if (rand > car) {
+				rand += 2;
+			} else {
+				break;
+			}
+		}
+		return rand;
+	}
+}
